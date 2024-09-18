030a2fe29b8ac0b07aad844ca1d8d76eb6dc1623
Mahbub Rahman <creative.mahbubrahman@gmail.com>
2024-09-18 23:33:19 +0600

    feat: add health-check route

--- blooddonor/api/api_v1/api.py
+++ blooddonor/api/api_v1/api.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from blooddonor.api.api_v1.endpoints import login, search, users
+from blooddonor.api.api_v1.endpoints import login, search, users, utils
 
 api_router = APIRouter()
 
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(search.router, prefix="/search", tags=["search"])
+api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 api_router.include_router(login.router, tags=["login"])
